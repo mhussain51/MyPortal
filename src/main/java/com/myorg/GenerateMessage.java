79e7a0500036dd5060c98122bbb1ba3d00de2321
mutahir <mutahirh@gmail.com>
2021-07-15 16:37:11 -0400

    updated the message

--- src/main/java/com/myorg/GenerateMessage.java
+++ src/main/java/com/myorg/GenerateMessage.java
@@ -3,7 +3,7 @@ package com.myorg;
 public class GenerateMessage {
 	public String getMessage(String name) {
 		
-		return  "Hello " + name + ", from DevOps Project.";
+		return  "Hello " + name + ", from DevOps Project. Just a demo.";
 	}
 
 }
